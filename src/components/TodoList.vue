ffb00cfa2e339f570253bbdbfc18304bdf163ce5
iykecode93 <boscoiyke67@gmail.com>
2024-07-05 16:04:51 +0100

    TodoList

---
+++ src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Todo List</h1>
+        <AddTodo @add-todo="addTodo" `/>
+    </div>
+</template>
+
+<script>
+import AddTodo from "./AddTodo.vue";
+import TodoItem from "./TodoItem.vue";
+
+    export default {
+        components: { AddTodo, TodoItem },
+        data() {
+            return {
+                todos: []
+            };
+        },
+
+        methods: {
+            addTodo(todo) {
+                this.todos
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
